e62eb2f0c2d36cee7f67376e4e70f63134af1bb2
CongDinh <dinhdcph14290@fpt.edu.vn>
2022-04-04 10:38:24 +0700

    feat: update

--- src/pages/ProductManager.tsx
+++ src/pages/ProductManager.tsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react'
 import { ProductType } from '../types/product'
-import { Table, Tag, Space } from 'antd';
+import { Table, Tag, Space, Button } from 'antd';
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
 type ProductManagerProps = {
     data: ProductType[],
@@ -28,8 +29,8 @@ const columns = [
     key: 'action',
     render: () => (
     <Space size="middle">
-        <a><EditOutlined /></a>
-        <a><DeleteOutlined /></a>
+        <Link to={`/edit`}><EditOutlined /></Link>
+        <Button type='primary' danger><DeleteOutlined /></Button>
     </Space>
     ),
 },
@@ -45,6 +46,28 @@ const ProductManager = (props: ProductManagerProps) => {
     return (
         <div className='container'>
             <Table columns={columns} dataSource={dataSource} />
+            {/* <table className='table table-bordered'>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+            {props.data && props.data.map((item, index) => {
+                return <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{item.name}</td>
+                        <td>
+                        <Link to={`/admin/product/${item.id}/edit`}>Edit</Link>
+                        <button onClick={() => props.onRemove(item.id)}>Remove</button>
+                        </td>
+                    </tr>
+            })}
+            
+            </tbody>
+      </table> */}
         </div>
     )
 }
